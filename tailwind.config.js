7d2bd8dae2dad8084500942f83982eee71241ca2
MT22HUGO <hugomartor07@gmail.com>
2026-03-05 18:00:01 +0100

    Instalo tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./frontend/src/**/*.{html,js}",
+    "./frontend/src/pages/**/*.html"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
